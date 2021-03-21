ff718cf8b70399129d971edccce1795ba9bf1623
Matheus Ribeiro <matheusribeiro161@gmail.com>
2021-03-21 00:54:09 -0300

    imapletação do menu e rodape

---
+++ src/app/rodepe/rodepe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rodepe',
+  templateUrl: './rodepe.component.html',
+  styleUrls: ['./rodepe.component.css']
+})
+export class RodepeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
